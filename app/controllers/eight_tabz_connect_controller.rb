1ed3a1bd3b6d130445784cb3591ac39ab953fe79
CHIKURA Shinsaku <chsh@thinq.jp>
2011-10-10 01:08:38 +0900

    Remove unnesessary files.

--- app/controllers/eight_tabz_connect_controller.rb
+++
@@ -1,9 +0,0 @@
-class EightTabzConnectController < ApplicationController
-  before_filter :authenticate_user!
-
-  def preferences
-    page = current_user.pages.where(app_id: params[:page_app_id]).first
-    raise "No preferences for #{params[:page_app_id]}" unless page
-    render text: 'Hello, Preferences!'
-  end
-end
--- app/helpers/eight_tabz_connect_helper.rb
+++
@@ -1,2 +0,0 @@
-module EightTabzConnectHelper
-end
